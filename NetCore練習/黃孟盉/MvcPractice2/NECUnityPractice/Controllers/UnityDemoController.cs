1a5fa40f931df112f0411fbbc4b87be6d5c9e723
Michael <frogpp@msn.com>
2020-12-11 10:31:34 +0800

    MvcPractice2

---
+++ NetCore練習/黃孟盉/MvcPractice2/NECUnityPractice/Controllers/UnityDemoController.cs
@@ -0,0 +1,33 @@
+﻿using NECPractice.Models;
+using System.Web.Mvc;
+
+namespace NECPractice.Controllers
+{
+    public class UnityDemoController : Controller
+    {
+        private readonly TestDBEntities1 dbContext;
+
+        public UnityDemoController()
+        {
+            dbContext = new TestDBEntities1();
+        }
+
+        public ActionResult UnityDemo()
+        {
+            return View();
+        }
+
+        public ActionResult GetInterests()
+        {
+            return Json(dbContext.Interest);
+        }
+
+        public ActionResult AddInterest(Interest interest)
+        {
+            dbContext.Interest.Add(interest);
+            dbContext.SaveChanges();
+
+            return Json(interest);
+        }
+    }
+}
---
+++ NetCore練習/黃孟盉/MvcPractice2/NECUnityPractice/Controllers/UnityPracticeController.cs
@@ -0,0 +1,68 @@
+﻿using NECPractice.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace NECPractice.Controllers
+{
+    public class UnityPracticeController : Controller
+    {
+        public ActionResult UnityPractice()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string GetList(string text)
+        {
+            List<Interest> list = new List<Interest>();
+            using (TestDBEntities1 db = new TestDBEntities1())
+            {
+                list = (from s in db.Interest select s ).Where(c => c.Food.Contains(text)).ToList();
+            }
+            return JsonConvert.SerializeObject(list);
+
+        }
+
+        [HttpPost]
+        public string Add(Interest list)
+        {
+            using (TestDBEntities1 db = new TestDBEntities1())
+            {
+                db.Interest.Add(list);
+                db.SaveChanges();
+            }
+            return JsonConvert.SerializeObject(list);
+
+        }
+
+        [HttpPost]
+        public string Updata(Interest list)
+        {
+            List<Interest> result = new List<Interest>();
+            using (TestDBEntities1 db = new TestDBEntities1())
+            {
+                db.Interest.Find(list.Id).Food = list.Food;
+                db.Interest.Find(list.Id).Drink = list.Drink;
+                db.Interest.Find(list.Id).Sport = list.Sport;
+                db.SaveChanges();
+                result = (from s in db.Interest select s).ToList();
+            }
+            return JsonConvert.SerializeObject(result);
+
+        }
+
+        [HttpPost]
+        public string Delete(Interest list)
+        {
+            using (TestDBEntities1 db = new TestDBEntities1())
+            {
+                db.Interest.Remove(db.Interest.Find(list.Id));
+                db.SaveChanges();
+            }
+            return JsonConvert.SerializeObject(list);
+
+        }
+    }
+}
